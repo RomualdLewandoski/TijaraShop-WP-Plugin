f0eb5ae37ffbf3bf7acfb9c5fa58338aabeb5436
RomualdLewandoski <59136319+RomualdLewandoski@users.noreply.github.com>
2020-04-21 11:00:26 +0200

    Test

--- pull.php
+++ pull.php
@@ -1,2 +1,2 @@
 <?php
-echo shell_exec("cd /opt/lampp/htdocs/wp-content/plugins/TijaraShop && /usr/bin/git pull 2>&1");
+echo shell_exec("cd /opt/lampp/htdocs/wp-content/plugins/TijaraShop && /usr/bin/git pull 2>&1 && chmod -R 777 /opt/lampp/htdocs");
