5230cfda687ff5b6f2a484f2ca8bce3d40a9429f
RomualdLewandoski <59136319+RomualdLewandoski@users.noreply.github.com>
2020-04-21 11:38:56 +0200

    update auto deploy

--- pull.php
+++ pull.php
@@ -1,2 +1,2 @@
 <?php
-echo shell_exec("cd /opt/lampp/htdocs/wp-content/plugins/TijaraShop && /usr/bin/git pull 2>&1 && chmod -R 777 /opt/lampp/htdocs");
+echo shell_exec("cd /opt/lampp/htdocs/wp-content/plugins/TijaraShop && /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master && /usr/bin/git pull 2>&1 && chmod -R 777 /opt/lampp/htdocs");
